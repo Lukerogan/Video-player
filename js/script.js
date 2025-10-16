ad92c4e3a484342eb90de11693166e8a6f6b950f
Luke <126lrogan@frhsd.com>
2025-10-16 08:36:41 -0400

    notdone

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
-document.getElementById("brainRot").onclick = function() {myFunction()};
+document.getElementById("brainRot").onclick = function() {brainrotFunction()};
+document.getElementById("jackie").onclick = function() {jackieFunction()};
+document.getElementById("jorkin").onclick = function() {jorkinFunction()};
+document.getElementById("brainRot").onclick = function() {brainrotFunction()};
+document.getElementById("brainRot").onclick = function() {brainrotFunction()};
 
-var x = document.getElementById("myVideo");
+var a = document.getElementById("myVideo");
+var b = document.getElementById("brainrotImg");
+var c = document.getElementById("jackieVid");
+var d = document.getElementById("jorkinIt");
+// var e =
+function brainrotFunction() { 
+  a.src = "img/brainrot.mp4"
+  a.load();
+if (a.src = "img/brainrot.mp4"){
+b.src = "img/sigma.jpg"
+}else if(a.src = "img/sigma.mp4"){
 
-function myFunction() { 
-  isSupp = x.canPlayType("video/mp4");
-  if (isSupp == "") {
-    x.src = "brainrot.mp4";
-  } else {
-    x.src = "brainrot.mp4";
-  }
-  x.load();
+}
 }
