da43e54c8e9febb9e0d719cef02f659785221f9c
Lukerogan <126Lrogan@frhsd.com>
2025-10-21 16:42:01 -0400

    Done

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
 document.getElementById("brainRot").onclick = function() {brainrotFunction()};
 document.getElementById("jackie").onclick = function() {jackieFunction()};
 document.getElementById("jorkin").onclick = function() {jorkinFunction()};
-document.getElementById("brainRot").onclick = function() {brainrotFunction()};
-document.getElementById("brainRot").onclick = function() {brainrotFunction()};
-document.getElementById("mainVid").onclick = function() {mainVid()};
+document.getElementById("Aaron").onclick = function() {AaronFunction()};
+document.getElementById("mainVid").onclick = function() {mainVidFunction()};
 
 var a = document.getElementById("myVideo");
 var b = document.getElementById("brainrotImg");
 var c = document.getElementById("jackieVid");
 var d = document.getElementById("jorkinIt");
-// var e =
+
 function brainrotFunction() { 
   a.src = "img/brainrot.mp4"
   a.load();
@@ -20,14 +19,29 @@ function jackieFunction() {
   a.load();
 }
 
-
-
-if (a.src = "img/brainrot.mp4"){
-b.src = "img/sigma.jpg"
-c.src = "img/jackiethumbnail.jpg"
-d.src = "jorkin.jpg"
-}else if(a.src = "img/sigma.mp4"){
-b.src = "img/brainrot.jpg"
-c.src = "img/jackiethumbnail.jpg"
-d.src = "jorkin.jpg"
+function jorkinFunction() {
+  a.src = "img/jorkin.mp4"
+  a.load();
 }
+
+function mainVidFunction() {
+  a.src = "img/sigma.mp4"
+  a.load();
+}
+
+function AaronFunction() {
+  a.src = "img/Aaron.mp4"
+  a.load();
+}
+
+
+
+// if (a.src = "img/brainrot.mp4"){
+// b.src = "img/sigma.jpg"
+// c.src = "img/jackiethumbnail.jpg"
+// d.src = "jorkin.jpg"
+// }else if(a.src = "img/sigma.mp4"){
+// b.src = "img/brainrot.jpg"
+// c.src = "img/jackiethumbnail.jpg"
+// d.src = "jorkin.jpg"
+// }
