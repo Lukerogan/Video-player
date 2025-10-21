d8d1c4fccc94dfe5ef01ea43fe6091367d17029c
Luke <126lrogan@frhsd.com>
2025-10-21 08:36:10 -0400

    do at home

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ document.getElementById("jackie").onclick = function() {jackieFunction()};
 document.getElementById("jorkin").onclick = function() {jorkinFunction()};
 document.getElementById("brainRot").onclick = function() {brainrotFunction()};
 document.getElementById("brainRot").onclick = function() {brainrotFunction()};
+document.getElementById("mainVid").onclick = function() {mainVid()};
 
 var a = document.getElementById("myVideo");
 var b = document.getElementById("brainrotImg");
@@ -12,9 +13,21 @@ var d = document.getElementById("jorkinIt");
 function brainrotFunction() { 
   a.src = "img/brainrot.mp4"
   a.load();
+}
+
+function jackieFunction() {
+  a.src = "img/jackievid.mp4"
+  a.load();
+}
+
+
+
 if (a.src = "img/brainrot.mp4"){
 b.src = "img/sigma.jpg"
+c.src = "img/jackiethumbnail.jpg"
+d.src = "jorkin.jpg"
 }else if(a.src = "img/sigma.mp4"){
-
-}
+b.src = "img/brainrot.jpg"
+c.src = "img/jackiethumbnail.jpg"
+d.src = "jorkin.jpg"
 }
